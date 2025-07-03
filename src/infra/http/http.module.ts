6f34ad601f0e04e3f55591cf15b66f825ba43902
Erick <erick.kuwahara79@gmail.com>
2025-07-03 18:43:04 -0300

    feat: add get-order-by-id controller

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -3,14 +3,14 @@ import { Module } from '@nestjs/common'
 import { DatabaseModule } from '../database/database.module'
 
 import { CreateOrderUseCase } from '@/application/order/use-cases/create-order'
+import { GetOrderByIdUseCase } from '@/application/order/use-cases/get-order-by-id'
 import { HealthCheckController } from './crontrollers/app/health-check.controller'
 import { CreateOrderController } from './crontrollers/order/create-order.controller'
+import { GetOrderByIdController } from './crontrollers/order/get-order-by-id.controller'
 
 @Module({
   imports: [DatabaseModule],
-  controllers: [HealthCheckController, CreateOrderController],
-  providers: [
-    CreateOrderUseCase,
-  ],
+  controllers: [HealthCheckController, CreateOrderController, GetOrderByIdController],
+  providers: [CreateOrderUseCase, GetOrderByIdUseCase],
 })
 export class HttpModule {}
--- src/infra/http/presenters/order-presenter.ts
+++ src/infra/http/presenters/order-presenter.ts
@@ -3,8 +3,17 @@ import { Order } from '@/domain/order/order'
 export class OrderPresenter {
   static toHTTP(order: Order) {
     return {
-      orderId: order.id,
+      id: order.id,
+      status: order.status,
       total: order.total,
+      createdAt: order.createdAt,
+      updatedAt: order.updatedAt,
+    }
+  }
+
+  static toMinimalHTTP(order: Order) {
+    return {
+      orderId: order.id,
     }
   }
 }
